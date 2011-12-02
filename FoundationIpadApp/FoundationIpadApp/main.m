7773cbaa75ad2fd5039170d94dcac123343b47c0
martin schueller <martin_schuller@martins-mbp.gnwc.private>
2011-12-01 17:00:47 -0800

    phoneGap project added

---
+++ FoundationIpadApp/FoundationIpadApp/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FoundationIpadApp
+//
+//  Created by default on 11-12-01.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
